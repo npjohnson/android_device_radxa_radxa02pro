4cc98bbd70f935c7c38919a6c8503724d565b56e
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-19 16:31:32 -0400

    radxa02pro: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_radxa02pro.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_radxa02pro-eng \
-    lineage_radxa02pro-user \
-    lineage_radxa02pro-userdebug
